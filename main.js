118a33b16ffa423fb2509343fc863b68119bca7e
hely15 <helydiaz65@gmail.com>
2025-05-14 19:20:26 -0500

    feat: :sparkles: Update registration text and add Firebase configuration and initialization

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//Credenciales de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCL2CctB5ULQg2tkWKKTX20-Aot0aGsVYw",
+    authDomain: "registrocon-97cb2.firebaseapp.com",
+    projectId: "registrocon-97cb2",
+    storageBucket: "registrocon-97cb2.firebasestorage.app",
+    messagingSenderId: "373644979789",
+    appId: "1:373644979789:web:5b08ef46eb8852c59fb604",
+    measurementId: "G-X5Q3RL90PV"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db   = firebase.firestore();
+
 
 //Funcion Iniciar Sesion con google
 function loginWithGoogle() {
