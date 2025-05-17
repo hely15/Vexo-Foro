5e7773017ff4659c70f5e5d01bf3f3b9072f97ec
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-05-17 16:34:33 -0500

    feat: :construction: proyect testing files with real repo fusion test#1

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function loginWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider)
         .then(result => {
-            window.location.href = "perfil.html";
+            window.location.href = "forum.html";
             console.log("Usuario autenticado:", { displayName, email, photoURL });
         })
         .catch(error => {
